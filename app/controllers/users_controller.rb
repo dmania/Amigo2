b1a73253ea34529f1b7dbea7861f091073ad2491
dmania <derina.man@gmail.com>
2015-03-07 19:12:24 -0500

    omniauth facebook working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,51 @@
 class UsersController < ApplicationController
 
   def index
+    @user = current_user
   end
 
   def login
+    @user = current_user
 
     if user_signed_in? 
-      # redirect_to user_path
-      # redirect_to @user
+    #redirect_to user_path
+    # redirect_to edit_user_path(:user)
+    redirect_to @user
     else
       render :login
     end
 
   end
 
-  def new
-  	@user = User.new
-    # binding.pry
+
+  def edit 
+    
+
+    @user = current_user
+    
+
   end
 
-  def create 
+  def update
     username = params[:user][:user_name]
     phone = params[:user][:user_phone]
     dob = Date.parse(params[:user][:user_birthday])
-    @user = current_user
 
+    @user = current_user
+    
     @user.user_name = username
     @user.user_phone = phone
     @user.user_birthday = dob
     @user.save
-
 
     redirect_to @user
 
   end
 
   # def create
-  #   dob = params[:user][:user_birthday]
-  #   dob = Date.parse(dob)
-  #   @user = User.new(safe_user_params)
+  #   dob = Date.parse(params[:user][:user_birthday])
+  #   @user = current_user
   #     @user.user_birthday = dob
-  #   binding.pry
   #     if @user.save
 	 #  	redirect to @user
 	 #  else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessible :password
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # devise :database_authenticatable, :registerable,
@@ -11,9 +12,10 @@ class User < ActiveRecord::Base
 def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image # assuming the user model has an image
+    user.encrypted_password = Devise.friendly_token[0,20]
+    user.user_name = auth.info.name   # assuming the user model has a name
+    # user.image = auth.info.image # assuming the user model has an image
+    # binding.pry
   end
 end
 
